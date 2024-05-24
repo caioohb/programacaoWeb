63afc7199781b4537dd2edcfacf38c2bc9af28c3
CAIO HENRIQUE BEMFICA DA ROCHA <caio.bemfica@a.ucb.br>
2024-05-24 10:11:18 -0300

    atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+let num1 = 1;
+let num2 = 2;
+
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+console.log(soma);
+console.log(sub);
+console.log(mult);
+console.log(div);
